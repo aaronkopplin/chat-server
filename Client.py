1b0c16f2700cfb5859f628caa8b427e45c28d5f5
aaron <kopplina@mail.gvsu.edu>
2020-10-07 15:39:06 -0400

    same script can send messages

--- Client.py
+++ Client.py
@@ -3,16 +3,48 @@ import socket
 HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 65432        # The port used by the server
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    while True:
-        received_message = s.recv(1024)
-        if received_message:
-            print("them > ", repr(received_message))
-        send_message = input("me > ")
-        s.sendall(send_message.encode())
 
-        if send_message == "q":
-            break
+def init_connection():
+    try:
+        # try to be the host
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.bind((HOST, PORT))
+        sock.listen()
+        messenger, address = sock.accept()
+        chat(messenger)
+        messenger.close()
+    except:
+        # there is already a host running
+        messenger = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        messenger.connect((HOST, PORT))
+        chat(messenger)
+        messenger.close()
 
-    s.close()
+
+def chat(messenger: socket):
+    messenger.sendall("data".encode())
+    received_message = messenger.recv(1024)
+    if received_message:
+        print(received_message.decode("utf-8"))
+
+
+init_connection()
+
+
+
+
+
+#
+# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+#     s.connect((HOST, PORT))
+#     while True:
+#         received_message = s.recv(1024)
+#         if received_message:
+#             print("them > ", repr(received_message))
+#         send_message = input("me > ")
+#         s.sendall(send_message.encode())
+#
+#         if send_message == "q":
+#             break
+#
+#     s.close()
