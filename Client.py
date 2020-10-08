9891636d359c1762e6e9b172e0e94911891b0582
aaron <kopplina@mail.gvsu.edu>
2020-10-07 20:41:29 -0400

    same script can send messages

--- Client.py
+++ Client.py
@@ -29,22 +29,3 @@ def chat(messenger: socket):
 
 
 init_connection()
-
-
-
-
-
-#
-# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-#     s.connect((HOST, PORT))
-#     while True:
-#         received_message = s.recv(1024)
-#         if received_message:
-#             print("them > ", repr(received_message))
-#         send_message = input("me > ")
-#         s.sendall(send_message.encode())
-#
-#         if send_message == "q":
-#             break
-#
-#     s.close()
